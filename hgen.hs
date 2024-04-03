2746cdd003355e5a399ea843ef8d140683b02c0b
Dylan Kneidel <dckneidel@ku.edu>
2024-04-03 11:41:51 -0500

    redoing parsing, keeping tag functions

--- hgen.hs
+++ hgen.hs
@@ -3,71 +3,26 @@ import Data.Char
 import Text.Parsec
 import Text.Parsec.String (Parser)
 
-
--- stmt ::= header | comment | text
-stmt :: Parser String
-stmt = try header
-    <|> try comment
-    <|> text
+-- file ::= (header | comment | text)*
+-- header ::= "#" number text
+-- comment ::= "//" text
+-- text ::= char* bold+ italic+ char*
 
 createHeaderTag :: String -> String -> String
 createHeaderTag x y = "<h" ++ x ++ ">" ++ y ++ "</h" ++ x ++ ">\n"
 
--- header ::= "#"* " " text
-header :: Parser String
-header = do
-    x <- many (char '#')
-    _ <- char ' '
-    createHeaderTag (show (length x)) <$> text
-
 -- comment ::= "//" text 
-comment :: Parser String
-comment = do
-    _ <- string "//"
-    x <- text
-    return ("<!-- " ++ x ++ " -->\n")
+commentTag :: String -> String
+commentTag x = "<!-- " ++ x ++ " -->\n"
 
-bold :: Parser String
-bold = do
-    _ <- try (string "**") <|> try (string "__")
-    content <- many (noneOf "*_")
-    _ <- try (string "**") <|> try (string "__")
-    return ("<b>" ++ content ++ "</b>")
+boldTag :: String -> String
+boldTag x = "<b>" ++ x ++ "</b>"
 
-italic :: Parser String
-italic = do
-    _ <- try (char '*') <|> try (char '_')
-    content <- many (noneOf "*_")
-    _ <- try (char '*') <|> try (char '_')
-    return ("<i>" ++ content ++ "</i>")
-
-text :: Parser String
-text = concat <$> manyTill (try bold <|> fmap return (noneOf "\n")) newline
+italicTag :: String -> String
+italicTag x = "<i>" ++ x ++ "</i>"
 
 baseHtml :: String -> String
 baseHtml text = "<html>\n<head>\n</head>\n<body>\n" ++ text ++ "\n</body>\n</html>"
 
-parseString :: String -> [String]
-parseString str =
-    case parse (stmt `sepBy` newline) "" str of
-        Left err -> ["Error: " ++ show err]
-        Right stmts -> stmts
-
-
-parseFile :: FilePath -> IO [String]
-parseFile filename = do
-    str <- readFile filename
-    return (parseString str)
-
-
--- take an input .md file and return an output .html file
 main :: IO ()
-main = do
-    args <- getArgs
-    case args of
-        [inFile, outFile] -> do
-            parsed <- parseFile inFile
-            let html = baseHtml (concat parsed)
-            putStrLn html
-            writeFile outFile html
-        _ -> putStrLn "Usage: hgen <markdown> <html>"
+main = putStrLn $ baseHtml $ createHeaderTag "1" "Hello, World!"
--- test.md
+++ test.md
@@ -6,4 +6,8 @@
 
 Headers can go until H6 (######) and only active if first non-whitespace char in line
 
+**Bold Text**
+
+*Italic Text*
+
 // comment block (must start with //)
